ecda23206dae57202c9a0be2f332be3c9633f5ed
Carter <75151313+CarterTheCoder@users.noreply.github.com>
2024-10-05 08:50:08 -0400

    Update and rename main.sh to make-units.sh

--- make-units.sh
+++ make-units.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 for container in $(sudo podman ps -a --format "{{.Names}}"); do
+
+  echo "Now creating unit file for container-${container}
+
   sudo podman generate systemd --name $container --files --new
   sudo mv ./container-${container}.service /etc/systemd/system/
   sudo systemctl enable /etc/systemd/system/container-${container}.service
+
+  echo "Finished creating unit file for container-${container}
 done
+
+echo "All done!"
